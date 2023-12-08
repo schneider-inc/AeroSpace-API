6c5565bc245ee95708de9664402f8f39d21cc804
schneider-inc <schneiderjus191@gmail.com>
2023-12-08 09:49:43 +0800

    pls

--- cli/gitHashGenerated.swift
+++ cli/gitHashGenerated.swift
@@ -1,2 +1,2 @@
-public let gitHash = "9a2c1df89544d7febcd44a051c0e57f00c5a76a1"
-public let gitShortHash = "9a2c1df"
+public let gitHash = "a5a0a22cb5c02bbe1f9bbd9a16457331c5332998"
+public let gitShortHash = "a5a0a22"
