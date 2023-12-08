c8039e27ba9cf14ebec137f17d0bd58e9e958812
schneider-inc <schneiderjus191@gmail.com>
2023-12-08 09:48:14 +0800

    removed get-workspace-monitors command

--- src/command/cli/GetWorkspaceMonitorsCommand.swift
+++
@@ -1,7 +0,0 @@
-struct GetWorkspaceMonitorsCommand: QueryCommand {
-    func run() -> String {
-        check(Thread.current.isMainThread)
-        return monitors.description.joined(" ")
-    }
-}
-
